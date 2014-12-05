decc94d1bc5609cf3a7d7b545456ad3b38bc5cd4
Matt Berube <vatara420@gmail.com>
2014-12-05 11:07:35 -0500

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Test : MonoBehaviour {
+	void Start () {
+		InputField input = GetComponent<InputField>();
+		input.text = "test";
+
+		input.enabled = false;
+		input.enabled = true;
+	}
+}
